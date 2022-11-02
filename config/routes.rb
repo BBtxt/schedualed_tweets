91f61ee49f1b8a3f9e7aa7e130e60c7201e37ce1
Brandon Baker <bbaker.sh@gmail.com>
2022-11-01 19:52:49 -0700

    refactored nav bar links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
 
-  get "/about", to: "about#index"
-  get "/", to: "main#index"
+  get "/about-us", to: "about#index", as: :about
+
+  root to: "main#index"
  
 end
